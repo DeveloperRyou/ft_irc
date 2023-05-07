409a094a1a63676e518ae4e63eb3383630291125
soyeong-lee <soylee@student.42seoul.kr>
2023-05-07 20:54:11 +0900

    add Header Guard & change in_channel data type & change send_to_Client Prototype

--- include/ft_irc.hpp
+++ include/ft_irc.hpp
@@ -1,3 +1,14 @@
+#ifndef FT_IRC_HPP
+# define FT_IRC_HPP
+
 #include <iostream>
+#include <stdexcept>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <vector>
+
 #include "Client.hpp"
 
+#endif
